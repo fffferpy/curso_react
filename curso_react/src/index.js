589d669edb13f3fda26772950f36826f05e49f76
Felix Gonzalez Aponte <felix.gonzalez@DIGIWEB.local>
2020-07-23 20:15:50 -0400

    nuevo componente Welcome

--- curso_react/src/index.js
+++ curso_react/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Welcome from './Welcome';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Welcome />
   </React.StrictMode>,
   document.getElementById('root')
 );
