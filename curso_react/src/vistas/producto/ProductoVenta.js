21511049db54a98f1a8838072d5b077230f12a5f
Felix Gonzalez Aponte <felix.gonzalez@DIGIWEB.local>
2020-07-31 19:32:57 -0400

    act. ProductoVenta.js

--- curso_react/src/vistas/producto/ProductoVenta.js
+++ curso_react/src/vistas/producto/ProductoVenta.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row, Col, Form, Button } from 'react-bootstrap';
+import { Row, Col, Form, Button, Table } from 'react-bootstrap';
 import { withRouter } from 'react-router-dom';
 
 
@@ -14,20 +14,32 @@ class ProductoVenta extends Component {
     render() {
         return (
             <>
+            <Form>
                 <Row style={{marginTop:"10px"}}> 
                     <Col><h2>Ventas</h2></Col>
                 </Row>
                 <Row>
-                    <Col xs={4} md={6}>
-                        <Form>
+                    <Col md={3}>
                         <Form.Group>
                                 <Form.Label>Fecha</Form.Label>
                                 <Form.Control type="date"  />
                                 {/* <Form.Text className="text-muted">
                                     Campo obligatorio
                                 </Form.Text> */}
+                         </Form.Group>
+                    </Col>
+                  
+                    <Col md={2}>
+                           <Form.Group>
+                                <Form.Label>Código Producto</Form.Label>
+                                <Form.Control type="number"  />
+                                {/* <Form.Text className="text-muted">
+                                    Campo obligatorio
+                                </Form.Text> */}
                             </Form.Group>
-                            
+                    </Col>
+
+                    <Col md={4}>
                             <Form.Group>
                                 <Form.Label>Producto</Form.Label>
                                 <Form.Control type="text" value={this.state.nombreProducto} onChange={(evento)=>{this.capturarTeclaProducto(evento)}} placeholder="Inserte nombre del producto" />
@@ -37,22 +49,19 @@ class ProductoVenta extends Component {
                                     Campo obligatorio
                                 </Form.Text> */}
                             </Form.Group>
-                            
-                            <Form.Group>
-                                <Form.Label>Código Producto</Form.Label>
-                                <Form.Control type="number"  />
-                                {/* <Form.Text className="text-muted">
-                                    Campo obligatorio
-                                </Form.Text> */}
-                            </Form.Group>
-
-                            <Form.Group>
+                    </Col>
+                    <Col md={3}>
+                             <Form.Group>
                                 <Form.Label>Precio Venta</Form.Label>
                                 <Form.Control type="number"  />
                                 {/* <Form.Text className="text-muted">
                                     Campo obligatorio
                                 </Form.Text> */}
                             </Form.Group>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={2}> 
 
                             <Form.Group>
                                 <Form.Label>Cantidad</Form.Label>
@@ -61,11 +70,41 @@ class ProductoVenta extends Component {
                                     Campo obligatorio
                                 </Form.Text> */}
                             </Form.Group>                         
-                        </Form>
-                    
-                    </Col>
-                    
+                       
+                    </Col>  
                 </Row>
+            </Form>
+            <Row>
+                <Col>
+                        <Table striped bordered hover>
+                                    <thead>
+                                        <tr>
+                                            <th>Código</th>
+                                            <th>Producto</th>
+                                            <th>Precio Compra</th>
+                                            <th>Precio Venta</th>
+                                            <th>Entradas</th>
+                                            <th>Salidas</th>
+                                            <th>Stock</th>
+                                            <th>Acciones</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>1</td>
+                                            <td>champ PLAT</td>
+                                            <td>45.000</td>
+                                            <td>70.000</td>
+                                            <td>20</td>
+                                            <td>15</td>
+                                            <td>5</td>
+                                            <td><a href='#' >Anular</a></td>
+                                        </tr>
+                                       
+                                    </tbody>
+                        </Table>
+                </Col>
+            </Row>
                 <Row>
                     <Col md={6}>
                         <Button variant="primary">Guardar</Button>{' '}
